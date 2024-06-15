c9560778b679b69dc346ca51d062e1a02f18d2ba
elaplas <eg.laplas@gmail.com>
2024-06-15 21:58:09 +0200

    Complete autograd class

--- autograd.py
+++ autograd.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Var:
     def __init__(self, val, name=""):
         self.data = val
@@ -17,6 +20,19 @@ class Var:
         res._children.add(self)
         res._children.add(other)
         return res
+    
+    def __sub__(self, other):
+        if type(other) != Var:
+            other = Var(other)
+        res = Var(self.data - other.data, "res")
+        def grad():
+            self.grad = res.grad
+            other.grad = -1*res.grad
+        res._grad_func = grad
+        res._children.add(self)
+        res._children.add(other)
+        return res
+            
         
     def __mul__(self, other):
         if type(other) != Var:
@@ -28,7 +44,31 @@ class Var:
         res._grad_func = grad
         res._children.add(self)
         res._children.add(other)
+        return res
+    
+    def __pow__(self, n):
+        res = Var(self.data*self.data, "res")
+        def grad():
+            self.grad = n*((self.data)**(n-1))*res.grad
+        res._grad_func = grad
+        res._children.add(self)
         return res   
+    
+    def tanh(self):
+        x = self.data
+        t = (math.exp(2*x)-1)/(math.exp(2*x)+1)
+        res = Var(t, "res")
+        def grad():
+            self.grad = (1 - (res.data**2))*res.grad
+        res._grad_func = grad
+        res._children.add(self)
+        return res
+    
+    def __rmul__(self, other):
+        return self*other
+    
+    def __radd__(self, other):
+        return self*other
     
     def backward(self):
         if self._grad_func is None:
@@ -47,23 +87,3 @@ class Var:
                         nodes.append(child)
                         visited.add(child)
             nodes.pop(0)
-                             
-
-w1 = Var(3, "w1")
-x1 = Var(2, "x1")
-b1 = Var(1, "b1")
-w2 = Var(4, "w2")
-b2 = Var(7, "b2")
-
-b1.name = "b1"
-y = (w1*x1) + b1
-z = (y*w2) + b2
-
-z.backward()
-
-print(w1.name, ": ", w1.grad)
-print(x1.name, ": ", x1.grad)
-print(b1.name, ": ", b1.grad)
-print(y.name, ": ", y.grad)
-print(w2.name, ": ", w2.grad)
-print(b2.name, ": ", b2.grad)
